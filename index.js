92c01878a6fc7766d4d424745c401f8dcbb83a50
Vishal Kumar <vishalkumargupta4546@gmail.com>
2025-12-25 12:31:27 +0530

    Fix MongoDB connection options for Render deploy

--- index.js
+++ index.js
@@ -496,10 +496,7 @@ app.use(errorHandler);
 
 // ========= DB Connection =========
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
